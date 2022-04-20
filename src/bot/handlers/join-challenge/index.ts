aced59aa4f4840a9aa225917307d9115ccfa1d1e
Dmytro <darnelbro@gmail.com>
2022-04-20 18:01:10 +0200

    fixed issue #3

--- src/bot/handlers/join-challenge/index.ts
+++ src/bot/handlers/join-challenge/index.ts
@@ -8,14 +8,17 @@ const joinChallengeHandler = async (ctx: Context) => {
   const currentChallenge = await getCurrentChallenge(chatId!);
 
   if (currentChallenge) {
-    const { id, username } = ctx.message!.from!;
+    const {
+      id, username, first_name, last_name,
+    } = ctx.message!.from!;
     const alreadyIn = challenge.isInChallenge(id!, currentChallenge);
+    const usernameStr = username ? `@${username}` : `${first_name} ${last_name || ''}`;
 
     if (currentChallenge.hasStarted) return ctx.reply('сорян другалёк, челлендж то уже начался', { reply_to_message_id: message_id });
 
     if (alreadyIn) return ctx.reply('ты шо глэк? ты уже в челендже', { reply_to_message_id: message_id });
 
-    await challenge.joinChallenge({ id, username: `@${username}` }, currentChallenge);
+    await challenge.joinChallenge({ id, username: `${usernameStr}` }, currentChallenge);
     ctx.reply('ок теперь ты в челлендже', { reply_to_message_id: message_id });
   } else {
     ctx.reply('ща поход челенджа нет', { reply_to_message_id: message_id });
